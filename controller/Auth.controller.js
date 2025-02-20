7fc42ddd1cfce321fcfdef26472b17ce47e689ff
TALHA IQBAL <134729186+ItsTalhaIqbal@users.noreply.github.com>
2025-02-20 19:55:41 +0500

    Update Auth.controller.js

--- controller/Auth.controller.js
+++ controller/Auth.controller.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../schema/user.schema.js";
 
 export const signup = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req;
 
   try {
     const dbUser = await User.findOne({ email });
