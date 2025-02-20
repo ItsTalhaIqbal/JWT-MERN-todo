bb72766c95b122b8378625edae4395741296bdad
TALHA IQBAL <134729186+ItsTalhaIqbal@users.noreply.github.com>
2025-02-20 19:56:17 +0500

    Update Auth.controller.js

--- controller/Auth.controller.js
+++ controller/Auth.controller.js
@@ -6,7 +6,7 @@ export const signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const dbUser = await User.findOne({  });
+    const dbUser = await User.findOne({ email });
     if (dbUser) {
       return res.status(400).send({ message: "Email already registerd" });
     }
